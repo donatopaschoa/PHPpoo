d93aeae19ae6065033d283955f81f900789fadbb
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2023-02-13 16:32:06 -0300

    POO PHP: Objeto - visibilidade (publico/privado)

--- aula02/Caneta.php
+++ aula02/Caneta.php
@@ -3,16 +3,15 @@
 class Caneta {
 
     // Atributos:
-    var $modelo;
-    var $cor;
-    var $ponta;
-    var $carga;
-    var $tampada;
+    public $modelo;
+    public $cor;
+    private $ponta;
+    protected $carga;
+    protected $tampada;
 
     // Métodos:
-    function rabiscar(){
+    public function rabiscar(){
         if ($this->tampada == true){ 
-        // >> comparando atributos dentro de uma classe, não colocar "$" e usar "$this->"
             echo "<p> Erro, não posso rabiscar</p>";
         }else{
             echo "<p>Estou rabiscando...</p>";
@@ -20,12 +19,11 @@ class Caneta {
         
     }
 
-    function tampar(){
+    public function tampar(){
         $this->tampada = true;
-        // Este método está modificando o atributo c/ "$this"
     }
 
-    function destampar(){
+    private function destampar(){
         $this->tampada = false;
     }
 }
--- aula02/index.php
+++ aula02/index.php
@@ -7,23 +7,20 @@
     <title>Curso básico de POO em PHP</title>
 </head>
 <body>
-    <?php
-        // Importando a classe:
-        require_once 'Caneta.php';
-
-        $c1 = new Caneta;
-        $c1->cor = "Azul";
-        $c1->ponta = 0.5;
-        $c1->destampar();
-        print_r($c1);
-
-        echo "<br>";
-
-        $c2 = new caneta;
-        $c2->cor = "Verde";
-        $c2->carga = 50;
-        $c2->tampar(); 
-        print_r($c2);
-    ?>
+    <pre>
+        <?php
+            // Importando a classe:
+            require_once 'Caneta.php';
+            $c1 = new Caneta;
+            $c1->modelo = "BIC cristal";
+            $c1->cor = "Azul";
+            //$c1->ponta = 0.5;
+            //$c1->carga = 99;
+            //$c1->tampada = true; >> não executa pois gera erro de permissão
+            $c1->rabiscar();
+            $c1->tampar(); // >> executa pois tampar() é um método público
+            print_r($c1);
+        ?>
+    </pre>
 </body>
 </html>
